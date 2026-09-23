b07b4fdd89d99db525909d1be5fea739b16625a9
BAKENLAB <keetasama.s2@gmail.com>
2026-09-24 08:23:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lab-hub-shell-v1';
+const CACHE = 'lab-hub-shell-v2';
 const SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/icon-maskable-512.png'];
 
 self.addEventListener('install', (event) => {
